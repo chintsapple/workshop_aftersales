7ec3c394ddcb3fc12cc36e4c4b08bea235ead1f8
Chintan Panchal <pancb@ymail.com>
2020-12-21 18:57:26 +0530

    developing application

--- models/wrksp.py
+++ models/wrksp.py
@@ -7,10 +7,20 @@ class Team(models.Model):
     _description = 'Team'
     _auto = True  # this by default true, here I have mentioned for understanding only, it creates table automatically
     _table = 'team'  # This is given so we explicitly give table a name else table name will be team_team
+    _order = 'name'  # used to show records in ascending order.
 
-    name = fields.Char('Name', required=True)
-    birthdate = fields.Date('Birthday', required=True)
-    age = fields.Integer('age')
+    name = fields.Char('First Name', required=True)
+    ls_name = fields.Char('Last Name', required=True)
+    address = fields.Text('Address', required=True)
+    per_address = fields.Text('Permanent Address')
+    pin_code = fields.Char('Pin Code', size=6)
+    email = fields.Char('Email')
+    # birthdate = fields.Date('Birthday', required=True)
+    birthdate = fields.Date('Birthday', default=fields.datetime(1980, 1, 1))
+    marital_status = fields.Selection([('single', 'Single'),
+                                       ('Married', 'Married'),
+                                       ('separated', 'Separated')], 'Marital Status')
+    age = fields.Integer('age', default=21)
     gender = fields.Selection([('male', 'Male'),
                                ('female', 'female')], 'Gender')
     high_edu = fields.Selection([('graduate', 'Graduate'),
