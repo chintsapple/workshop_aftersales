480ce0deded17ea181cf7686f92cc308e478a7f1
Chintan Panchal <pancb@ymail.com>
2020-12-22 12:12:04 +0530

    Many2one relation added

--- models/wrksp.py
+++ models/wrksp.py
@@ -9,9 +9,10 @@ class Team(models.Model):
     _table = 'team'  # This is given so we explicitly give table a name else table name will be team_team
     _order = 'name'  # used to show records in ascending order.
 
+    # Personal info:
     name = fields.Char('First Name', required=True)
     ls_name = fields.Char('Last Name', required=True)
-    address = fields.Text('Address', required=True)
+    res_address = fields.Text('Residential Address', required=True)
     per_address = fields.Text('Permanent Address')
     pin_code = fields.Char('Pin Code', size=6)
     email = fields.Char('Email')
@@ -31,14 +32,23 @@ class Team(models.Model):
     msg = fields.Text('Message')
     template = fields.Html('Template')
 
+    # Job Info
+    title = fields.Char('Title')
+    emp_id = fields.Char('Employee ID', size=4)
+    department_id = fields.Many2one('team.department', 'Department')
+    work_loc = fields.Selection([('ahmedabad', 'Ahmedabad'),
+                                 ('baroda', 'Baroda'),
+                                 ('anand', 'Anand'),
+                                 ('surat', 'Surat')], 'Work Location')
 
 
-    # Integer
-    # ◦ Float
-    # ◦ Boolean
-    # ◦ Char
-    # ◦ Text
-    # ◦ Html
-    # ◦ Date
-    # ◦ Datetime
-    # ◦ Selection
+class Department(models.Model):
+
+    _name = 'team.department'
+    _description = 'Department'
+
+    _rec_name = 'dep_name'
+
+    dep_name = fields.Char('Department Name')
+    dep_code = fields.Char('Code', size=4)
+
